4b8e3c4f43099e5bc509fd99aebae10a691ab544
comin <comingdoomsday@gmail.com>
2020-12-29 19:01:26 +0200

    testin git

--- src/components/Profile/Profile__postList/Profile__postList.jsx
+++ src/components/Profile/Profile__postList/Profile__postList.jsx
@@ -1,14 +1,7 @@
 import classes from './Profile__postList.module.css'
 import Profile__postList_post from "./Profile__postList_post/Profile__postList_post";
+import PostsMassive from "../../../index.js"
 
-
-let PostsMassive = [
-    {id: 0, user_name: 'Georgo Biba', post_content: 'asd2133', likesAmount: 1023},
-    {id: 1, user_name: 'Geo22rgo Biba', post_content: '2sad133', likesAmount: 3911},
-    {id: 2, user_name: 'Geor11g333o Biba', post_content: '213asas3', likesAmount: 4911},
-    {id: 3, user_name: 'Georg333o Biba', post_content: '2133ddsa', likesAmount: 5911},
-    {id: 4, user_name: 'Georg1333o Biba', post_content: '22asdqqq133', likesAmount: 123911}
-]
 
 let postsElements = PostsMassive.map(p => <Profile__postList_post id={p.id}
                                                                   user_name={p.user_name}
@@ -16,8 +9,8 @@ let postsElements = PostsMassive.map(p => <Profile__postList_post id={p.id}
                                                                   likesAmount={p.likesAmount}/>);
 
 
-const Profile__postList = () => {
-    return <div className={classes.posts}>
+const Profile__postList = (props) => {
+    return <div className={classes.general}>
         My Posts
         <br/>
         <br/>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+let PostsMassive = [
+    {id: 0, user_name: 'Georgo Biba', post_content: 'asd2133', likesAmount: 1023},
+    {id: 1, user_name: 'Geo22rgo Biba', post_content: '2sad133', likesAmount: 3911},
+    {id: 2, user_name: 'Geor11g333o Biba', post_content: '213asas3', likesAmount: 4911},
+    {id: 3, user_name: 'Georg333o Biba', post_content: '2133ddsa', likesAmount: 5911},
+    {id: 4, user_name: 'Georg1333o Biba', post_content: '22asdqqq133', likesAmount: 123911}
+]
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +26,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default {PostsMassive};
