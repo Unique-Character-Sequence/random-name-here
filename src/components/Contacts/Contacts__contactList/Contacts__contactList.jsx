2e98cac805036385d7acdb646e0ca4673abe6c9e
Unique-Character-Sequence <comingdoomsday@gmail.com>
2021-03-05 21:05:08 +0200

    Продолжаю понимать!

--- src/components/Contacts/Contacts__contactList/Contacts__contactList.jsx
+++ src/components/Contacts/Contacts__contactList/Contacts__contactList.jsx
@@ -1,33 +1,49 @@
 import classes from './Contacts__contactList.module.css'
 import Contacts__contactList_nameplate from "./Contacts__contactList_nameplate/Contacts__contactList_nameplate";
 import * as axios from "axios"
+import * as react from "react";
 
 
-const Contacts__contactList = (props) => {
-    if (props.UsersArray.length === 0) {
+class Contacts__contactList extends react.Component {
+    getUsers = () => {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(
                 response => {
-                    props.addUserToState(response.data)
-                    })
-                }
-// Здесь мы передаём в компонент данные из пропсов
-    let contactsElements = props.UsersArray.map(p => <Contacts__contactList_nameplate id={p.id}
-                                                                                      deleteContact={props.deleteContact}
-                                                                                      addContact={props.addContact}
-                                                                                      chatContact={props.chatContact}
-                                                                                      user_name={p.name}
-                                                                                      added={p.followed}
-                                                                                      user_img={p.photos.small}/>);
+                    this.props.addUserToState(response.data)
+                })
+    }
 
-    return <div className={classes.general}>
-        Search_form:
-        <br/>
-        Contacts:
-        <br/>
-        <br/>
-        {contactsElements}
-    </div>
+// Здесь мы передаём в компонент данные из пропсов
+    contactsElements = this.props.UsersArray.map(p => <Contacts__contactList_nameplate id={p.id}
+                                                                                       deleteContact={this.props.deleteContact}
+                                                                                       addContact={this.props.addContact}
+                                                                                       chatContact={this.props.chatContact}
+                                                                                       user_name={p.name}
+                                                                                       added={p.followed}
+                                                                                       user_img={p.photos.small}/>);
+
+    constructor(props) {
+        super(props)
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(
+                response => {
+                    this.props.addUserToState(response.data)
+                })
+    }
+
+    render() {
+        return <div className={classes.general}>
+            Search_form:
+            <br/>
+            Contacts:
+            <br/>
+            <br/>
+            <button onClick={this.getUsers}>Get Users</button>
+            <br/>
+            <br/>
+            {this.contactsElements}
+        </div>
+    }
 }
 
 
